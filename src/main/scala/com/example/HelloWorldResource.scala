1acffa0da635f26cac247622ea783033fd61f43c
jay.shah <jay.shah@workday.com>
2017-07-17 10:23:47 -0700

    more endpoints

--- src/main/scala/com/example/HelloWorldResource.scala
+++ src/main/scala/com/example/HelloWorldResource.scala
@@ -4,18 +4,26 @@ import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.Response
 
 import com.codahale.metrics.annotation.Timed
+import org.joda.time.DateTime
 
 @Path("/example")
 class HelloWorldResource() {
 
   @GET
+  @Produces(Array(APPLICATION_JSON))
+  def default: Response = {
+    Response.ok.entity(Map("message" -> "Scala rocks!", "today" -> DateTime.now)).build()
+  }
+
+  @GET
   @Timed
+    @Path ("/user")
   @Produces(Array(MediaType.APPLICATION_JSON))
   def sayHello(): Response = {
     Response.ok.entity(new User("jshah", "test@test.com")).build()
   }
-
 }
